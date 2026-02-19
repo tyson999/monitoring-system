1071f56f9d1b17025cfd9d1177da2bbc6bfe625b
Tanish Bhardwaj <tanish0188@gmail.com>
2026-02-19 15:34:11 +0530

    secret key

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 import os
 SECRET_KEY = 'django-insecure-b!$p#cz)=iwawe0lidc=fc03lj^!04ky*&w+cae#itq0l#zbzh'
-SECRET_KEY = os.environ.get("SECRET_KEY = 'django-insecure-b!$p#cz)=iwawe0lidc=fc03lj^!04ky*&w+cae#itq0l#zbzh'")
+# SECRET_KEY = os.environ.get("SECRET_KEY = 'django-insecure-b!$p#cz)=iwawe0lidc=fc03lj^!04ky*&w+cae#itq0l#zbzh'")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -84,10 +84,11 @@ import os
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
-        conn_max_age=600)   
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "db.sqlite3",
     }
+}
 
 
 # Password validation
