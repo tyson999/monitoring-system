35f1cdf7512ab12c440ba4c7589634313995a463
Tanish Bhardwaj <tanish0188@gmail.com>
2026-02-20 01:51:07 +0530

    chanhe

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+
+
 import os
+print("DATABASE_URL:", os.environ.get("DATABASE_URL"))
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
