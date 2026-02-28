5b33bf01d2e63d89a81b94c8161357c83eb63071
Tanish Bhardwaj <tanish0188@gmail.com>
2026-02-28 12:08:14 +0530

    DB

--- config/settings.py
+++ config/settings.py
@@ -88,10 +88,10 @@ import os
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
